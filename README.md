97105310484e7ea0a92db40f913b7c31471f431a
Jeremy <jectaunscripted@gmail.com>
2023-11-06 09:34:31 +0100

    Update package.json and README.md, remove unused dependencies and files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rmbg
+# Remove Background (rmbg)
+Technology that removes the background of images using the inspyrenet 2022 ported to a node package.
+
+## How to use?
+1. Install our [bun](https://bun.sh/) (fast runtime with built-in typescript support) i prefer using.
+2. Install packages i prefer [pnpm](https://pnpm.io/) so `pnpm install`.
+3. Upload images in the /images directory.
+4. Run either by `pnpm run dev` or `bun run`.
+5. For support, questions or else join our [Discord](https://discord.com/invite/f4KEs5TVz2).
+
+## Dependencies
+- [transparent-background](https://www.npmjs.com/package/transparent-background) by [@makidoll](https://github.com/makidoll) - used to remove the background of images.
+- [fs](https://github.com/npm/fs) - used to write the data in .json files.
+- [path](https://www.npmjs.com/package/path) - used to find paths easily.
--- index.ts
+++ index.ts
@@ -1,32 +1,37 @@
 import * as fs from "fs";
 import * as path from "path";
-import { Rembg } from "rembg-node";
-import sharp from "sharp";
+import { transparentBackground } from "transparent-background";
 
-const imagesDir = path.join(__dirname, 'images');
-const processedDir = path.join(__dirname, 'processed');
+const imagesDir = path.join(__dirname, "images");
+const processedDir = path.join(__dirname, "processed");
 
 const removeBackground = async (filename: string) => {
-  const input = sharp(path.join(imagesDir, filename));
-  const rembg = new Rembg({
-    logging: true,
+  const input = await fs.promises.readFile(path.join(imagesDir, filename));
+  const output = await transparentBackground(input, "png", {
+    fast: false, // uses a 1024x1024 model by default, setting to true uses a 384x384 model instead (faster but lower quality).
   });
-  const output = await rembg.remove(input);
-  await output.toFile(path.join(processedDir, filename));
+  await fs.promises.writeFile(path.join(processedDir, filename), output);
+  await fs.promises.unlink(path.join(imagesDir, filename)); 
   console.log(`Processed ${filename}`);
 };
 
 const start = Date.now();
+console.log(`Getting the model and processing all images in ${imagesDir}\n`);
 
 if (!fs.existsSync(processedDir)) {
   fs.mkdirSync(processedDir);
 }
 
-Promise.all(fs.readdirSync(imagesDir).filter((filename) => {
-  return filename.endsWith('.png') || filename.endsWith('.jpg');
-}).map((filename) => {
-  return removeBackground(filename);
-})).then(() => {
+Promise.all(
+  fs
+    .readdirSync(imagesDir)
+    .filter((filename) => {
+      return filename.endsWith(".png") || filename.endsWith(".jpg");
+    })
+    .map((filename) => {
+      return removeBackground(filename);
+    })
+).then(() => {
   const end = Date.now();
-  console.log(`Processed all images in ${end - start}ms`);
+  console.log(`\nProcessed all images in ${end - start}ms`);
 });
